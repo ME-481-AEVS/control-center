9afcf7980f10e946827be870f3a272167e9144c8
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-04-03 22:19:45 -1000

    Add bootstrap, add basic layout

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 
 const app = express(); // init app
+
+app.use('/scripts/bootstrap', express.static(`${__dirname}/node_modules/bootstrap/dist/`));
+app.use('/scripts/bootstrap-icons', express.static(`${__dirname}/node_modules/bootstrap-icons/font/`));
+
+app.use(express.static(`${__dirname}/views`)); // load views
+app.use(express.static(`${__dirname}/views/components`)); // load components
+app.use(express.static(`${__dirname}/public`)); // define public folder
+
 app.set('view engine', 'ejs'); // set view engine to ejs
 
 // home route
