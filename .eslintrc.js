cc9586754c65010ac715e15be95f276b2fe14e53
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-04-03 17:46:04 -1000

    Init npm

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    _: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'global-require': 'off',
+    'arrow-parens': 'off',
+    'func-names': 'off',
+    'linebreak-style': 'off',
+    'max-len': ['error', 120],
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-restricted-syntax': 'off',
+    'no-return-assign': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+};
