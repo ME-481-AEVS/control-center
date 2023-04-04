43eddbcdd1464e52d16b265e66fea6518f2a3f2b
DOT <owen.bramley03@gmail.com>
2023-04-03 18:18:35 -1000

    added sum stuff

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-console.log('hello control center');
+// control route
+app.get('/', (req, res) => {
+  res.render('control');
+});
 
 // start server
 app.listen(port, () => console.log(`Server started on port ${port}.`));
