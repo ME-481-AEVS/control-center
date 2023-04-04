d05b37bcf96b0c1e0e5f0be6a903b89fa866b98d
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-04-03 18:10:19 -1000

    Basic structure

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express(); // init app
+app.set('view engine', 'ejs'); // set view engine to ejs
+
+// home route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 console.log('hello control center');
+
+// start server
+app.listen(port, () => console.log(`Server started on port ${port}.`));
