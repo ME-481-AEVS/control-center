84a46c6a44bdbdcd3ec8ec970e9193fc421aad13
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-04-07 17:06:47 -1000

    Style updates

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express(); // init app
 
 app.use('/scripts/bootstrap', express.static(`${__dirname}/node_modules/bootstrap/dist/`));
 app.use('/scripts/bootstrap-icons', express.static(`${__dirname}/node_modules/bootstrap-icons/font/`));
+app.use('/scripts/jquery', express.static(`${__dirname}/node_modules/jquery/dist/`));
 
 app.use(express.static(`${__dirname}/views`)); // load views
 app.use(express.static(`${__dirname}/views/components`)); // load components
